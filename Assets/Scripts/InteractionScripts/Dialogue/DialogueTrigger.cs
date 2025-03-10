b2a65022d4414aad24e1fe5747d22fd99a07b2e8
Monika Ortiz <monika.ortiz.aof@gmail.com>
2025-03-09 21:18:40 -0700

    ink scripts

--- Assets/Scripts/InteractionScripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/InteractionScripts/Dialogue/DialogueTrigger.cs
@@ -20,12 +20,12 @@ public class DialogueTrigger : MonoBehaviour
 
     private void Update()
     {
-        if(playerInRange)
+        if(playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying)
         {
             visualCue.SetActive(true);
             if(InputManager.GetInstance().GetInteractPressed())
             {
-                Debug.Log(inkJSON.text);
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
             }
         }
         else
